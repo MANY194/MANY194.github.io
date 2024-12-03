d14435b7ae60aa50991d2bee582fcb8d30db63d5
MANY194 <manuelantoniogutierrezbernal@gmail.com>
2024-12-02 21:09:13 -0600

    Add files via upload

---
+++ imagenes.js
@@ -0,0 +1,49 @@
+let frutas = [];
+let reverso = "reverso.png";
+
+function cargarImagenes() {
+
+	frutas[1] = "ajo.jpg";
+	frutas[0] = "aguacate.jpg";
+	frutas[2] = "alcachofa.jpg";
+	frutas[3] = "apio.jpg";
+	frutas[4] = "apio.png";
+	frutas[5] = "calabaza.png";
+	frutas[6] = "cebolla.jpg";
+	frutas[7] = "cereza.jpg";
+	frutas[8] = "coco.jpg";
+	frutas[9] = "frambuesa.png";
+	frutas[10] = "fresa.png";
+	frutas[11] = "granada.jpeg";
+	frutas[12] = "jengibre.jpg";
+	frutas[13] = "kiwi.jpg";
+	frutas[14] = "lechuga.jpeg";
+	frutas[15] = "lechugaiceberg.png";
+	frutas[16] = "lima.jpg";
+	frutas[17] = "/limon.jpeg";
+	frutas[18] = "mandarina.jpeg";
+	frutas[19] = "manzanaroja.jpg";
+	frutas[20] = "manzanaverde.jpg";
+	frutas[21] = "melocoton.png";
+	frutas[22] = "melon.jpeg";
+	frutas[23] = "millo.png";
+	frutas[24] = "mora.jpeg";
+	frutas[25] = "naranja.png";
+	frutas[26] = "papa.jpg";
+	frutas[27] = "pepino.jpeg";
+	frutas[28] = "pepino.png";
+	frutas[29] = "pera.jpg";
+	frutas[30] = "pimientoamarillo.jpeg";
+	frutas[31] = "pimientorojo.png";
+	frutas[32] = "piña.jpeg";
+	frutas[33] = "platano.jpeg";
+	frutas[34] = "puerro.png";
+	frutas[35] = "remolacha.jpg";
+	frutas[36] = "sandia.png";
+	frutas[37] = "tomate.png";
+	frutas[38] = "uvablanca.jpg";
+	frutas[39] = "uvaroja.jpg";
+	frutas[40] = "zanahoria.png";
+	frutas[41] = "uvaazul.jpeg";
+
+}
